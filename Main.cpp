3409798ef492283fbf825e42b93907f859bbf01e
Дмитрий Гераськов <167875309+Destryz@users.noreply.github.com>
2024-07-17 13:13:35 +0300

    Complete Task 14

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "Helpers.h"
+#include <string>
 
 int main()
 {
-	float a, b;
+	/*float a, b;
 
 	std::cout << "input a: ";
 	std::cin >> a;
@@ -11,6 +12,11 @@ int main()
 	std::cin >> b;
 
 	double result = squaresum(a, b);
+	std::cout << "The square of the sum  = " << result << std::endl;*/
 
-	std::cout << "The square of the sum  = " << result << std::endl;
+	std::string str;
+	str = "Hello Skillbox";
+
+	std::cout << "String value is '" << str << "'. String length is '" << str.length() << "'. First symbol is '" << str[0] << "'. Last symbol is '" << str[str.length() - 1] << "'.";
+
 }
