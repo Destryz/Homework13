7d23dbf623bb026aae19cf1f23c7286d3f98e8c4
Дмитрий Гераськов <167875309+Destryz@users.noreply.github.com>
2024-08-01 16:50:13 +0300

    complete task 15

--- Main.cpp
+++ Main.cpp
@@ -2,8 +2,40 @@
 #include "Helpers.h"
 #include <string>
 
-int main()
+void FindOddNumbers(int Limit, bool IsOdd)
 {
+	int i;
+	if (!IsOdd)
+	{
+		i = 2;
+		std::cout << "Even numbers in a given limit\n";
+	}
+	else
+	{
+		i = 1;
+		std::cout << "Odd numbers in a given limit\n";
+
+	}
+
+	for (i; i <= Limit; i += 2)
+	{
+		std::cout << i << std::endl;
+	}
+
+}
+
+int main()
+{  
+	int n;
+	bool IsOdd;
+	
+	std::cout << "input Limit\n";
+	std::cin >> n;
+	std::cout << "Is odd? (0/1)\n";
+	std::cin >> IsOdd;
+
+	FindOddNumbers(n, IsOdd);
+
 	/*float a, b;
 
 	std::cout << "input a: ";
@@ -12,11 +44,11 @@ int main()
 	std::cin >> b;
 
 	double result = squaresum(a, b);
-	std::cout << "The square of the sum  = " << result << std::endl;*/
+	std::cout << "The square of the sum  = " << result << std::endl;
 
 	std::string str;
 	str = "Hello Skillbox";
 
-	std::cout << "String value is '" << str << "'. String length is '" << str.length() << "'. First symbol is '" << str[0] << "'. Last symbol is '" << str[str.length() - 1] << "'.";
+	std::cout << "String value is '" << str << "'. String length is '" << str.length() << "'. First symbol is '" << str[0] << "'. Last symbol is '" << str[str.length() - 1] << "'."; */
 
 }
